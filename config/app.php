13a8f57154358aaab06b085a1c649a110effe32f
Ingrid Ayala <ingridayala_94@hotmail.com>
2017-10-11 19:56:58 -0600

    configuraciones y cosas que actualizar

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+         Laravel\Tinker\TinkerServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Collective\Html\HtmlServiceProvider::class,
+
 
     ],
 
@@ -225,6 +228,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Input' => Illuminate\Support\Facades\Input::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Form'=>Collective\Html\FormFacade::class,
+        'Html'=>Collective\Html\HtmlFacade::class,
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,38 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+  $users=DB::select('select * from users');
+  $cuenta= count($users);
+  if ($cuenta==0) {
+      return view('primerUsuario');
+  } else {
+      return view('login');
+  }
 
+
+});
+
+Route::get('/inicio', function () {
+    return view('fondo');
+});
+
+Route::get('pass', function () {
+    return view('recuperacion');
+});
 
 Route::resource('categorias','CategoriaController');
 Route::resource('proveedores','ProveedorController');
 Route::resource('compras','CompraController');
 Route::resource('productos','ProductoController');
 Route::resource('users','UserController');
-Route::resource('users','UserController');
+
+Route::resource('login','LoginController');
+Route::get('logout','LoginController@logout');//salir
+Route::post('/pass/correo','LoginController@correo');
+
+Route::match(['get','post'],'destroyProveedor/{id}','ProveedorController@destroy');
+Route::match(['get','post'],'destroyCategoria/{id}','CategoriaController@destroy');
+Route::match(['get','post'],'destroyProducto/{id}','ProductoController@destroy');
+Route::match(['get','post'],'destroyCompra/{id}','CompraController@destroy');
+Route::match(['get','post'],'destroyUser/{id}','UserController@destroy');
